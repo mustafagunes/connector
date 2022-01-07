a36c7a8eff75eda9b64717ff9732e1de514a2bb4
Mustafa GÜNEŞ <gunes149@gmail.com>
2022-01-07 17:18:00 +0300

    fix: package.swift (#2)
    * update package.swift
    
    * update package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Connector",
+    name: "connector",
     platforms: [
         .iOS(.v9)
     ],
